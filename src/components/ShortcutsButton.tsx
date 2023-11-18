9d64e51d59394666b9b5dc7b11c33334d79e3369
avega <avega@DETI-0942>
2023-11-18 10:49:02 -0500

    Add

--- src/components/ShortcutsButton.tsx
+++ src/components/ShortcutsButton.tsx
@@ -1,11 +1,34 @@
 import { Button } from "@/components/ui/button"
 import { Icon, IconKey } from "@/components/icon/Icon";
+import clsx from "clsx";
+
+
+interface BottomBarBtnProps {
+    icon: IconKey;
+    label: string;
+}
+const BottomBarButton = ({ icon, label }: BottomBarBtnProps) => {
+    return (
+        <>
+            <div
+                className={clsx(
+                    "flex flex-col align-middle justify-center w-14 h-14 p-0"
+                )}
+            >
+                <div className={clsx("flex align-middle justify-center")}>
+                    <Icon type={icon} />
+                </div>
+                <p className={clsx("text-center")}>{label}</p>
+            </div>
+        </>
+    );
+};
 
 const ShortcutsButton = () => {
 
     return (<>
         <section>
-            <Button>boton</Button>
+            <BottomBarButton icon="plin" label="Transferir"></BottomBarButton>
         </section>
     </>)
 }
